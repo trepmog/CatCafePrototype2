1a2cc582b81942111bdc7bcc15221daf1167305e
trepmog <joelgompert@yahoo.com>
2024-04-30 16:43:02 -0700

    Fix for match success (or match fail) message getting stuck on
    The timer was apparently added because without it the message would never appear because it was hidden immediately. But the timer meant that if you picked up the cat too quickly, then the message would stay on.
    
    For now the message just displays for a set amount of time, and does not require a key-press.
    Alternatively we could do the hide on cat pickup.

--- Assets/_CatCafe/01_Scripts/ConvoView.cs
+++ Assets/_CatCafe/01_Scripts/ConvoView.cs
@@ -7,162 +7,157 @@ using TMPro;
 
 public class ConvoView : MonoBehaviour
 {
-    public Customer customer;
-    private GameObject convoBG;
-    private GameObject matchSuccess;
-    private GameObject matchFail;
-    private GameObject reward;
-    private GameObject thankYou;
-    private GameObject continueText;
-    public TextMeshProUGUI conversationText;
-    public TextMeshProUGUI nameText;
-    private CustomerDatabase customerDB;
-    public MatchMaker matchMaker;
-    private int currentTextIndex = 0;
-    private string customerId;
-    private bool isConvoActive = false;
-    private bool isFirstInteract = true;
-    private bool matchResultShown = false;
-    private float delayBeforeHide = 0.5f; // 500 milliseconds before allowing hide
-    private float timer;
+	public Customer customer;
+	private GameObject convoBG;
+	private GameObject matchSuccess;
+	private GameObject matchFail;
+	private GameObject reward;
+	private GameObject thankYou;
+	private GameObject continueText;
+	public TextMeshProUGUI conversationText;
+	public TextMeshProUGUI nameText;
+	private CustomerDatabase customerDB;
+	public MatchMaker matchMaker;
+	private int currentTextIndex = 0;
+	private string customerId;
+	private bool isConvoActive = false;
+	private bool isFirstInteract = true;
+	private const float delayBeforeHide = 2.0f;
+	private float timer;
 
-    void Start()
-    {
-        LoadCustomerData();
-        customer.OnInteract += ShowConvoUI;
-        matchMaker.OnMatchResult += ShowMatchResult;
-        convoBG = transform.Find("ConvoBG").gameObject;
-        matchSuccess = transform.Find("MatchSuccess").gameObject;
-        matchFail = transform.Find("MatchFailText").gameObject;
-        thankYou = transform.Find("ThankYou").gameObject;
-        reward = transform.Find("Reward").gameObject;
-        continueText = transform.Find("ContinueText").gameObject;
-        convoBG.SetActive(false);
-    }
+	void Start()
+	{
+		LoadCustomerData();
+		customer.OnInteract += ShowConvoUI;
+		matchMaker.OnMatchResult += ShowMatchResult;
+		convoBG = transform.Find( "ConvoBG" ).gameObject;
+		matchSuccess = transform.Find( "MatchSuccess" ).gameObject;
+		matchFail = transform.Find( "MatchFailText" ).gameObject;
+		thankYou = transform.Find( "ThankYou" ).gameObject;
+		reward = transform.Find( "Reward" ).gameObject;
+		continueText = transform.Find( "ContinueText" ).gameObject;
+		convoBG.SetActive( false );
+	}
 
-    void Update()
-    {
-        // Handle key press to cycle through the conversation
-        if (isConvoActive && Input.GetKeyDown(KeyCode.E))
-        {
-            //Debug.Log("Tried to show next piece of dialogue");
-            NextConvoText();
-        }
-        if ((matchSuccess.activeSelf || matchFail.activeSelf) && Input.GetKeyDown(KeyCode.E) && matchResultShown)
-        {
-            // Only hide the results if the Player has seen the results
-            if (timer <= 0)
-            {
-                HideMatchResult();
-                timer = delayBeforeHide; // Reset timer
-            }
-        }
-        // Update the timer
-        if (timer > 0)
-        {
-            timer -= Time.deltaTime;
-        }
-    }
+	void Update()
+	{
+		// Handle key press to cycle through the conversation
+		if ( isConvoActive && Input.GetKeyDown( KeyCode.E ) )
+		{
+			//Debug.Log("Tried to show next piece of dialogue");
+			NextConvoText();
+		}
 
-    private void OnDisable()
-    {
-        customer.OnInteract -= ShowConvoUI;
-    }
+		if ( matchSuccess.activeSelf || matchFail.activeSelf )
+		{
+			if ( timer > 0 )
+			{
+				timer -= Time.deltaTime;
+			}
+			else
+			{
+				HideMatchResult();
+			}
+		}
+	}
 
-    public void ShowConvoUI(string givenId)
-    {
-        customerId = givenId;
-        // Shows convo BG
-        SetActiveConvo(true);
-        DisplayCustomerDemands();
-    }
+	private void OnDisable()
+	{
+		customer.OnInteract -= ShowConvoUI;
+	}
 
-    public void ShowMatchResult(bool result)
-    {
-        //Debug.Log("Match Success is: " + matchSuccess.activeSelf);
-        if (result)
-        {
-            matchSuccess.SetActive(true);
-            thankYou.SetActive(true);
-            continueText.SetActive(true);
-            // This is to prevent the Update from hiding the results right away
-            timer = delayBeforeHide;
-            matchResultShown = true;
-        }
-        else
-        {
-            Debug.Log("It thinks it's a fail");
-            matchFail.SetActive(true);
-            continueText.SetActive(true);
-            // This is to prevent the Update from hiding the results right away
-            timer = delayBeforeHide;
-            matchResultShown = true;
-        }
-    }
+	public void ShowConvoUI( string givenId )
+	{
+		customerId = givenId;
+		// Shows convo BG
+		SetActiveConvo( true );
+		DisplayCustomerDemands();
+	}
 
-    public void HideMatchResult()
-    {
-        matchSuccess.SetActive(false);
-        thankYou.SetActive(false);
-        matchFail.SetActive(false);
-        continueText.SetActive(false);
-    }
+	public void ShowMatchResult( bool result )
+	{
+		//Debug.Log("Match Success is: " + matchSuccess.activeSelf);
+		if ( result )
+		{
+			matchSuccess.SetActive( true );
+			thankYou.SetActive( true );
+			//continueText.SetActive( true );
+			// This is to prevent the Update from hiding the results right away
+			timer = delayBeforeHide;
+		}
+		else
+		{
+			Debug.Log( "It thinks it's a fail" );
+			matchFail.SetActive( true );
+			//continueText.SetActive( true );
+			// This is to prevent the Update from hiding the results right away
+			timer = delayBeforeHide;
+		}
+	}
 
-    private void SetActiveConvo(bool isActive)
-    {
-        isConvoActive = isActive;
-        convoBG.SetActive(isActive);
-        // Resets if setting convo to inactive
-        if (!isActive)
-        {
-            currentTextIndex = 0;
-            isFirstInteract = true;
-        }
-    }
+	public void HideMatchResult()
+	{
+		matchSuccess.SetActive( false );
+		thankYou.SetActive( false );
+		matchFail.SetActive( false );
+		continueText.SetActive( false );
+	}
 
-    void LoadCustomerData()
-    {
-        TextAsset jsonData = Resources.Load<TextAsset>("CustomerText");
-        customerDB = JsonUtility.FromJson<CustomerDatabase>(jsonData.text);
-    }
+	private void SetActiveConvo( bool isActive )
+	{
+		isConvoActive = isActive;
+		convoBG.SetActive( isActive );
+		// Resets if setting convo to inactive
+		if ( !isActive )
+		{
+			currentTextIndex = 0;
+			isFirstInteract = true;
+		}
+	}
 
-    public void DisplayCustomerDemands()
-    {
-        foreach (CustomerEntry customerEntry in customerDB.customers)
-        {
-            if (customerEntry.id == customerId)
-            {
-                if (currentTextIndex < customerEntry.conversationTexts.Length)
-                {
-                    // Cycle to the next piece of dialogue
-                    var convoEntry = customerEntry.conversationTexts[currentTextIndex];
-                    nameText.text = convoEntry.speaker + ":";
-                    conversationText.text = convoEntry.text;
-                }
-                else
-                {
-                    SetActiveConvo(false); // End conversation if we run out of texts
-                }
-            }
-        }
-    }
+	void LoadCustomerData()
+	{
+		TextAsset jsonData = Resources.Load<TextAsset>( "CustomerText" );
+		customerDB = JsonUtility.FromJson<CustomerDatabase>( jsonData.text );
+	}
 
-    public void NextConvoText()
-    {
-        if (isConvoActive)
-        {
-            // Displays the first piece of dialogue on first interaction
-            if (isFirstInteract)
-            {
-                DisplayCustomerDemands();
-                isFirstInteract = false;
-            }
-            else
-            {
-                // Cycles to next piece of dialogue if not the first interaction
-                currentTextIndex++;
-                DisplayCustomerDemands();
-            }
-        }
-    }
+	public void DisplayCustomerDemands()
+	{
+		foreach ( CustomerEntry customerEntry in customerDB.customers )
+		{
+			if ( customerEntry.id == customerId )
+			{
+				if ( currentTextIndex < customerEntry.conversationTexts.Length )
+				{
+					// Cycle to the next piece of dialogue
+					var convoEntry = customerEntry.conversationTexts[currentTextIndex];
+					nameText.text = convoEntry.speaker + ":";
+					conversationText.text = convoEntry.text;
+				}
+				else
+				{
+					SetActiveConvo( false ); // End conversation if we run out of texts
+				}
+			}
+		}
+	}
+
+	public void NextConvoText()
+	{
+		if ( isConvoActive )
+		{
+			// Displays the first piece of dialogue on first interaction
+			if ( isFirstInteract )
+			{
+				DisplayCustomerDemands();
+				isFirstInteract = false;
+			}
+			else
+			{
+				// Cycles to next piece of dialogue if not the first interaction
+				currentTextIndex++;
+				DisplayCustomerDemands();
+			}
+		}
+	}
 }
